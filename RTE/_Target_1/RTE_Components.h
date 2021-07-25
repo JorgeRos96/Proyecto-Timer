9569e3eeb2738a036fac16d4331b03778d303a41
JorgeRos96 <86102864+JorgeRos96@users.noreply.github.com>
2021-07-25 11:38:03 +0200

    Add files via upload

--- RTE/_Target_1/RTE_Components.h
+++ RTE/_Target_1/RTE_Components.h
@@ -16,6 +16,17 @@
  */
 #define CMSIS_device_header "stm32f4xx.h"
 
+/*  Keil::CMSIS Driver:USART:2.13 */
+#define RTE_Drivers_USART1              /* Driver USART1 */
+        #define RTE_Drivers_USART2              /* Driver USART2 */
+        #define RTE_Drivers_USART3              /* Driver USART3 */
+        #define RTE_Drivers_USART4              /* Driver USART4 */
+        #define RTE_Drivers_USART5              /* Driver USART5 */
+        #define RTE_Drivers_USART6              /* Driver USART6 */
+        #define RTE_Drivers_USART7              /* Driver USART7 */
+        #define RTE_Drivers_USART8              /* Driver USART8 */
+        #define RTE_Drivers_USART9              /* Driver USART9 */
+        #define RTE_Drivers_USART10             /* Driver USART10 */
 /*  Keil::Device:STM32Cube Framework:Classic:1.7.9 */
 #define RTE_DEVICE_FRAMEWORK_CLASSIC
 /*  Keil::Device:STM32Cube HAL:Common:1.7.9 */
@@ -32,6 +43,8 @@
 #define RTE_DEVICE_HAL_RCC
 /*  Keil::Device:STM32Cube HAL:TIM:1.7.9 */
 #define RTE_DEVICE_HAL_TIM
+/*  Keil::Device:STM32Cube HAL:USART:1.7.9 */
+#define RTE_DEVICE_HAL_USART
 /*  Keil::Device:Startup:2.6.3 */
 #define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
 
